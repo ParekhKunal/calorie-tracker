89d280fcfd5eb6801376a1d44720f6e4d7fd048e
parekhkunal <parekhkunal09@gmail.com>
2022-10-29 19:00:05 +0530

    first commit:

---
+++ build/precache-manifest.d4091e06bc982763e2b68396b849530c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e262954eb38c5e4d81deea6f3e06a886",
+    "url": "/calorie-tracker/index.html"
+  },
+  {
+    "revision": "b3c83eb5bf08859fdfae",
+    "url": "/calorie-tracker/static/css/main.6cfff724.chunk.css"
+  },
+  {
+    "revision": "c38f867000b1da7d3d70",
+    "url": "/calorie-tracker/static/js/2.25db011e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calorie-tracker/static/js/2.25db011e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c83eb5bf08859fdfae",
+    "url": "/calorie-tracker/static/js/main.ac2401e0.chunk.js"
+  },
+  {
+    "revision": "484da5113f7678b5f2e9",
+    "url": "/calorie-tracker/static/js/runtime-main.154238ea.js"
+  }
+]);
